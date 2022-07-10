06e42555f0e0b787efa12427c60a830d86b4b40f
José Maria Macedo <jose_macedo@live.com>
2022-07-10 17:23:40 -0300

    Adicionando a quantidade de combustível
    Adicionando a quantidade de combustivel que cada carro terá.

--- carro.py
+++ carro.py
@@ -1,8 +1,10 @@
 class Carro():
-    def __init__(self, cor, qtd_portas, tipo_combustivel, potencia):
+    def __init__(self, cor, qtd_portas, tipo_combustivel, potencia, qtd_combustivel):
         self.cor = cor
         self.qtd_portas = qtd_portas
         self.tipo_combustivel = tipo_combustivel
         self.potencia = potencia
+        self.qtd_combustivel = qtd_combustivel
+        
         
         
