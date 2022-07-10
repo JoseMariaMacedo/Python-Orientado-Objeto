94b927a949411dc4154615cd6bfe015f4fcd0d89
José Maria Macedo <jose_macedo@live.com>
2022-07-10 18:22:18 -0300

    Criando outro objeto
    Criando outro objeto.

--- codigosOrientadosObjetos.py
+++ codigosOrientadosObjetos.py
@@ -5,3 +5,6 @@ uno_vermelho.ligar()
 uno_vermelho.abastecer()
 uno_vermelho.abastecer()
 print(f"A quantidade de combustivel do carro e: {uno_vermelho.qtd_combustivel} litros")
+
+ford_preto = carro.Carro("preto", 4,"Flex", 1.5, 0, False)
+ford_preto.desligar()
