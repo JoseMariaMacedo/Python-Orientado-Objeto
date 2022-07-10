f65a8877c3a923255ea3462ddc84893b19767f44
José Maria Macedo <jose_macedo@live.com>
2022-07-10 18:40:18 -0300

    verificando a velocidade do veículo
    chamada de função que incrementa a velocidade do veículo.

--- codigosOrientadosObjetos.py
+++ codigosOrientadosObjetos.py
@@ -1,10 +1,12 @@
 import carro
 
-uno_vermelho = carro.Carro("vermelhor", 4,"Flex", 1.0, 0, False)
+uno_vermelho = carro.Carro("vermelhor", 4,"Flex", 1.0, 0, False, 0)
 uno_vermelho.ligar()
 uno_vermelho.abastecer(21)
 uno_vermelho.abastecer(10)
 print(f"A quantidade de combustivel do carro e: {uno_vermelho.qtd_combustivel} litros")
 
-ford_preto = carro.Carro("preto", 4,"Flex", 1.5, 0, False)
-ford_preto.desligar()
+ford_preto = carro.Carro("preto", 4,"Flex", 1.5, 0, False, 0)
+ford_preto.ligar()
+ford_preto.acelerar(35)
+print(f"A velocidade do carro e: {ford_preto.velocidade} Km/h")
