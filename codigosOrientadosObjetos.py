1ffd24a04893dcdf04f3e98a12c2b5f2579220a8
José Maria Macedo <jose_macedo@live.com>
2022-07-10 18:20:23 -0300

    Adicionando mais ações ao carro
    Adicionando mais atributos à variável.

--- codigosOrientadosObjetos.py
+++ codigosOrientadosObjetos.py
@@ -1,4 +1,7 @@
 import carro
 
 uno_vermelho = carro.Carro("vermelhor", 4,"Flex", 1.0, 0, False)
-print(uno_vermelho.ligar())
+uno_vermelho.ligar()
+uno_vermelho.abastecer()
+uno_vermelho.abastecer()
+print(f"A quantidade de combustivel do carro e: {uno_vermelho.qtd_combustivel} litros")
