b17385309341ffba7f8078e11a1656535245b2aa
José Maria Macedo <jose_macedo@live.com>
2022-07-10 18:30:52 -0300

    criando um novo atributo
    Adicionando o atributo de velocidade

--- carro.py
+++ carro.py
@@ -4,13 +4,14 @@ from tkinter.tix import Tree
 
 
 class Carro():
-    def __init__(self, cor, qtd_portas, tipo_combustivel, potencia, qtd_combustivel, is_ligado):
+    def __init__(self, cor, qtd_portas, tipo_combustivel, potencia, qtd_combustivel, is_ligado, velociade ):
         self.cor = cor
         self.qtd_portas = qtd_portas
         self.tipo_combustivel = tipo_combustivel
         self.potencia = potencia
         self.qtd_combustivel = qtd_combustivel
         self.is_ligado = is_ligado
+        self.velociade = velociade
         
     def abastecer(self, qtd_combustivel):
         self.qtd_combustivel += qtd_combustivel
